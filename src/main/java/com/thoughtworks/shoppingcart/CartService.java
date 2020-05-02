1f3609c8d3c870d1be8d2a8bba837a6731811924
kumud garg <kumud.garg@kumuds-MacBook-Pro.local>
2020-05-02 15:03:29 +0530

    4.1:Refactored: Added a single product into cart to get total price

--- src/main/java/com/thoughtworks/shoppingcart/CartService.java
+++ src/main/java/com/thoughtworks/shoppingcart/CartService.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.shoppingcart;
+
 import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,23 +11,32 @@ public class CartService {
 
     private List<CartItem> cartItems = new ArrayList<>();
 
+    private double totalPrice;
 
-    public void addToCart(Product product, int quantity) {
-        CartItem existingCartItem = findCartItem(product.getName());
-        if(existingCartItem != null)
-            existingCartItem.increaseQuantity(quantity);
-        else {
-            CartItem newCartItem = new CartItem(product, quantity);
-            cartItems.add(newCartItem);
-        }
-    }
 
     private CartItem findCartItem(String name) {
         return cartItems.stream().filter(cartItem -> cartItem.getName() == name).findFirst().orElse(null);
     }
 
+    private double updateTotalPrice() {
+        totalPrice = cartItems.stream().mapToDouble(cartItem -> cartItem.getPrice()).sum();
+        return totalPrice;
+    }
+
+    public void addToCart(Product product, int quantity) {
+        CartItem existingCartItem = findCartItem(product.getName());
+        if (existingCartItem != null)
+            existingCartItem.increaseQuantity(quantity);
+        else {
+            CartItem newCartItem = new CartItem(product, quantity);
+            cartItems.add(newCartItem);
+        }
+
+        updateTotalPrice();
+    }
+
     public double getTotal() {
-        return cartItems.stream().mapToDouble(cartItem -> cartItem.getPrice()).sum();
+        return totalPrice;
     }
 
     @Override
@@ -33,7 +44,6 @@ public class CartService {
         Gson gson = new Gson();
         return gson.toJson(cartItems);
     }
-
 
 
 }
